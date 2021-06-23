8e9579fb0d7cdf4a3ce139b4d4877c6341ce9e69
Thomas Konnemann <kick.butts@gmx.de>
2021-06-23 08:02:53 +0200

    correction of typo

--- collectData.py
+++ collectData.py
@@ -22,15 +22,15 @@ import requests
 datum = yesterday.strftime('%d.%m.%Y')
 
 df_inzidenzen_Landkreise=pd.read_csv("https://raw.githubusercontent.com/semohr/risikogebiete_deutschland/master/assets/data/data_latest.csv")
-df_inzidenzen_Landkreise.to_csv('/data/inzidenz'+now.strftime('%d.%m.%Y')+'.csv', encoding='utf-8', index=False, header = True)
+df_inzidenzen_Landkreise.to_csv('data/inzidenz'+now.strftime('%d.%m.%Y')+'.csv', encoding='utf-8', index=False, header = True)
 df_inzidenzen_Landkreise['date']= datum
 #print(df_inzidenzen_Landkreise)
 
 df_Inzidenzen_Muenster=df_inzidenzen_Landkreise.loc[df_inzidenzen_Landkreise["Landreis ID"].isin([5515])]
 print(df_Inzidenzen_Muenster)
 
-df_Inzidenzen_Muenster.to_csv('/data/inzidenzenMuenster.csv', encoding='utf-8', index=False, mode='a', header = False)
-path = '/data'
+#df_Inzidenzen_Muenster.to_csv('/data/inzidenzenMuenster.csv', encoding='utf-8', index=False, mode='a', header = False)
+#path = '/data'
 
 files = os.listdir(path)
 
